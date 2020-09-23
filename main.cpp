b185d703120a01d6e1dad5ce2dc9cc82ba6465cc
luciana1faur <luciana_faur@yahoo.com>
2020-09-23 12:06:30 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b, suma;
+
+  cout << "Introduceti primul numar: ";
+  cin >> a;
+
+  cout << "Introduceti al doilea numar: ";
+  cin >> b;
+
+  suma = a +b;
+
+  cout << "Suma numerelor a si b este: " << suma << endl;
+
+  return 0;
+  
+}
